07c2265b9e3e3724f172e6a24cbf7410d90a32f3
Benjamin <benjamin.benni06@gmail.com>
2016-01-11 16:13:19 +0100

    Add roles
    Add role json attribute into protocol and check authorization into /songs

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,7 +3,8 @@ var Schema = mongoose.Schema;
 
 var UserSchema   = new Schema({
     email: String,
-    password: String
+    password: String,
+    role:String
 });
 
 module.exports = mongoose.model('User', UserSchema);
