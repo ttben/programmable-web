7d2e65533f8749dd4b965c035756c07b08b9db05
Hugo Simond <hugosimond@gmail.com>
2016-01-14 11:26:13 +0100

    Lol C bo

--- frontend/app/scripts/components/waveform.dir.js
+++ frontend/app/scripts/components/waveform.dir.js
@@ -6,30 +6,39 @@ angular.module('waveform-directive', [])
         buffer: '='
       },
       link: function (scope, element) {
-        // draw the waveform (only once)
-        var context = element[0].getContext('2d');
-        var width = element[0].width;
-        var height = element[0].height;
-        var data = scope.buffer.getChannelData(0);
-        var step = Math.floor(data.length / width);
-        var amp = height / 2;
-        var color = "#0000FF";
+        var color = "#" + ((1 << 24) * Math.random() | 0).toString(16);
 
-        context.clearRect(0, 0, width, height);
-        if (color)
-          context.fillStyle = color;
-        for (var i = 0; i < width; i++) {
-          var min = 1.0;
-          var max = -1.0;
-          for (var j = 0; j < step; j++) {
-            var datum = data[(i * step) + j];
-            if (datum < min)
-              min = datum;
-            if (datum > max)
-              max = datum;
+        /**
+         * draw the waveform (only once)
+         */
+        var draw = function () {
+          var context = element[0].getContext('2d');
+          var width = element[0].width = element.parent().width();
+          var height = element[0].height;
+          var data = scope.buffer.getChannelData(0);
+          var step = Math.floor(data.length / width);
+          var amp = height / 2;
+
+          context.clearRect(0, 0, width, height);
+          if (color)
+            context.fillStyle = color;
+          for (var i = 0; i < width; i++) {
+            var min = 1.0;
+            var max = -1.0;
+            for (var j = 0; j < step; j++) {
+              var datum = data[(i * step) + j];
+              if (datum < min)
+                min = datum;
+              if (datum > max)
+                max = datum;
+            }
+            context.fillRect(i, (1 + min) * amp, 1, Math.max(1, (max - min) * amp));
           }
-          context.fillRect(i, (1 + min) * amp, 1, Math.max(1, (max - min) * amp));
         }
+
+        scope.$watch(function () {
+            return element.parent().width();
+        }, function(){draw()}, true);
       }
     };
   });
