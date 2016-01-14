65b80845a1b019f6a5393a01bcb40ea96934bf15
Garance <garance.vallat@gmail.com>
2016-01-14 12:04:51 +0100

    connexion is handled !

---
+++ frontend/app/scripts/controllers/disconnected.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Garance on 14/01/2016.
+ */
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name programmableWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the programmableWebApp
+ */
+angular.module('programmableWebApp')
+  .controller('DisconnectedCtrl',['$scope', '$location', function ($scope, $location) {
+
+
+  }]);
---
+++ frontend/app/scripts/controllers/header.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name programmableWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the programmableWebApp
+ */
+angular.module('programmableWebApp')
+  .controller('HeaderCtrl', ['$scope', '$cookies', 'User', '$location', '$rootScope', function ($scope, $cookies, User, $location, $rootScope) {
+
+      if (typeof($cookies.get('token')) !== 'undefined') {
+        $location.path('/home');
+      }
+
+
+  //  $scope.user = {"email": "", "pwd": ""};
+
+    $scope.login = function (user) {
+      User.authenticate(user.usernameOrEmail, user.password, function (token) {
+          $rootScope.header = "default";
+          $location.path('/home');
+        },
+        function (error) {
+          console.log('error... :(');
+        })
+    };
+
+    $scope.logout = function() {
+
+      $rootScope.header = "home";
+      $location.path('/');
+    }
+  }]);
