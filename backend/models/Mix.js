cd56eaa88fe1d284569c20f92bab74487bbba967
Benjamin <benjamin.benni06@gmail.com>
2016-01-18 09:33:10 +0100

    Add Mix model
    Add Mix model according to given requirements describeb here https://jsonblob.com/568e3608e4b01190df47cb1b. Changes made : authorID is a string not a number.

---
+++ backend/models/Mix.js
@@ -0,0 +1,18 @@
+var mongoose     = require('mongoose');
+var Schema = mongoose.Schema;
+
+var MixSchema   = new Schema({
+    authorID:String,
+    originalTitle:String,
+    originalArtist:String,
+    date:[],
+    comments:[],
+    rating:Number
+});
+
+MixSchema.methods.addComment = function(commentDocument) {
+    this.comments.push(commentDocument);
+};
+
+var Mix = mongoose.model('Mix', MixSchema);
+
