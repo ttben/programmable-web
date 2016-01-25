49a38ab2149ec5172faa2de0a4bea7822b41ff72
Hugo Simond <hugosimond@gmail.com>
2016-01-25 15:20:16 +0100

    kill l'audioContext on destroy

--- frontend/app/scripts/components/audioplayer.js
+++ frontend/app/scripts/components/audioplayer.js
@@ -4,11 +4,11 @@ angular.module('audioPlayer-directive', [])
       restrict: 'E',
       scope: {},
       controller: function ($scope) {
-        $scope.saveMyMix = function() {
+        $scope.saveMyMix = function () {
           console.log('yepeeeeeeeeeeeeeeeee');
-          Music.createMix($cookies.token, 'hugo', $scope.info._id, 'mon super mix', $scope.tracks, function(successAnswer) {
+          Music.createMix($cookies.token, 'hugo', $scope.info._id, 'mon super mix', $scope.tracks, function (successAnswer) {
             console.log('yepeeeeeeeeeeeeeeeee');
-          }, function(error) {
+          }, function (error) {
             console.log(error);
           });
         };
@@ -20,135 +20,143 @@ angular.module('audioPlayer-directive', [])
 
         console.log('in the directive yo');
 
-          $scope.info = $cookies.music;
-          $scope.tracks = [];
-          for (var i = 0; i <  $scope.info.tracks.length; i++) {
-            $http({
-              method: 'GET',
-              url:  $scope.info.tracks[i].uri,
-              responseType: "arraybuffer"
-            }).success((function (i) {
-              return function (data) {
-                console.log('get a track success');
-                // on success : get the track buffer and keep the nodes in memory
-                $scope.audioContext.decodeAudioData(data, function (buffer) {
-                  var track = {"name": $scope.info.tracks[i].name, "volume": 1};
-                  track.source = $scope.audioContext.createBufferSource();
-                  track.source.buffer = buffer;
-                  track.gainNode = $scope.audioContext.createGain();
-                  track.trebleFilter = $scope.audioContext.createBiquadFilter();
-                  track.trebleFilter.type = "highshelf";
-                  track.trebleFilter.frequency.value = 2000;
-                  track.bassFilter = $scope.audioContext.createBiquadFilter();
-                  track.bassFilter.type = "lowshelf";
-                  track.bassFilter.frequency.value = 200;
-                  track.panNode = $scope.audioContext.createStereoPanner();
-                  track.source.connect(track.gainNode);
-                  track.gainNode.connect(track.trebleFilter);
-                  track.trebleFilter.connect(track.bassFilter);
-                  track.bassFilter.connect(track.panNode);
-                  track.panNode.connect($scope.audioContext.destination);
-                  $scope.tracks.push(track);
-                  $scope.start();
-                });
-              }
-            })(i));
+        $scope.info = $cookies.music;
+        $scope.tracks = [];
+        for (var i = 0; i < $scope.info.tracks.length; i++) {
+          $http({
+            method: 'GET',
+            url: $scope.info.tracks[i].uri,
+            responseType: "arraybuffer"
+          }).success((function (i) {
+            return function (data) {
+              if(!$scope.audioContext)return;
+              console.log('get a track success');
+              // on success : get the track buffer and keep the nodes in memory
+              $scope.audioContext.decodeAudioData(data, function (buffer) {
+                if(!$scope.audioContext)return;
+                var track = {"name": $scope.info.tracks[i].name, "volume": 1};
+                track.source = $scope.audioContext.createBufferSource();
+                track.source.buffer = buffer;
+                track.gainNode = $scope.audioContext.createGain();
+                track.trebleFilter = $scope.audioContext.createBiquadFilter();
+                track.trebleFilter.type = "highshelf";
+                track.trebleFilter.frequency.value = 2000;
+                track.bassFilter = $scope.audioContext.createBiquadFilter();
+                track.bassFilter.type = "lowshelf";
+                track.bassFilter.frequency.value = 200;
+                track.panNode = $scope.audioContext.createStereoPanner();
+                track.source.connect(track.gainNode);
+                track.gainNode.connect(track.trebleFilter);
+                track.trebleFilter.connect(track.bassFilter);
+                track.bassFilter.connect(track.panNode);
+                track.panNode.connect($scope.audioContext.destination);
+                $scope.tracks.push(track);
+                $scope.start();
+              });
+            }
+          })(i));
+        }
+
+        /**
+         * checks if all the tracks are loaded : start the tracks if so
+         */
+        $scope.start = function () {
+          if ($scope.tracks.length == $scope.info.tracks.length) {
+            $scope.tracksLoaded = true;
+
+            for (var i = 0; i < $scope.tracks.length; i++) {
+              $scope.tracks[i].source.start();
+            }
           }
+        };
 
-          /**
-           * checks if all the tracks are loaded : start the tracks if so
-           */
-          $scope.start = function () {
-            if ($scope.tracks.length == $scope.info.tracks.length) {
-              $scope.tracksLoaded = true;
+        /**
+         * tells audio elements to play/pause
+         */
+        $scope.playpause = function () {
+          for (var i = 0; i < $scope.tracks.length; i++) {
+            $scope.playing ? $scope.audioContext.suspend() : $scope.audioContext.resume();
+          }
+          $scope.playing = !$scope.playing;
+        };
 
-              for (var i = 0; i < $scope.tracks.length; i++) {
-                $scope.tracks[i].source.start();
+        /**
+         * called when the global volume or one of the tracks volumes is changed
+         */
+        $scope.volumeChanged = function (track) {
+          if (track && !track.muted) {
+            track.gainNode.gain.value = track.volume * $scope.globalVolume;
+            return;
+          }
+          for (var i = 0; i < $scope.tracks.length; i++) {
+            if ($scope.tracks[i].muted) {
+              $scope.tracks[i].gainNode.gain.value = 0;
+            } else {
+              $scope.tracks[i].gainNode.gain.value = $scope.tracks[i].volume * $scope.globalVolume;
+            }
+          }
+        };
+
+        $scope.mute = function (track) {
+          track.muted = !track.muted;
+          $scope.volumeChanged();
+        };
+
+        /**
+         * called when the user "travels through time"
+         * @param timeSelected
+         */
+        $scope.timeChanged = function (timeSelected) {
+          for (var i = 0; i < $scope.tracks.length; i++) {
+            $scope.tracks[i].source.stop();
+            var buffer = $scope.tracks[i].source.buffer;
+            $scope.tracks[i].source = $scope.audioContext.createBufferSource();
+            $scope.tracks[i].source.buffer = buffer;
+            $scope.tracks[i].source.connect($scope.tracks[i].gainNode);
+            $scope.tracks[i].source.start(0, timeSelected);
+            $scope.currentTime = timeSelected;
+            $scope.offset = $scope.audioContext.currentTime - $scope.currentTime;
+          }
+        };
+
+        /**
+         * called when the user wants to listen one single track
+         * @param track
+         */
+        $scope.silenceAskedFrom = function (track) {
+          var silence = false;
+          if (!track.muted) {
+            for (var i = 0; i < $scope.tracks.length; i++) {
+              if ($scope.tracks[i] != track && !$scope.tracks[i].muted) {
+                silence = true;
+                break;
               }
             }
-          };
+          } else {
+            silence = true;
+          }
+          track.muted = false;
+          for (var i = 0; i < $scope.tracks.length; i++) {
+            if ($scope.tracks[i] === track)continue;
+            $scope.tracks[i].muted = silence;
+          }
+          $scope.volumeChanged();
+        };
 
-          /**
-           * tells audio elements to play/pause
-           */
-          $scope.playpause = function () {
-            for (var i = 0; i < $scope.tracks.length; i++) {
-              $scope.playing ? $scope.audioContext.suspend() : $scope.audioContext.resume();
-            }
-            $scope.playing = !$scope.playing;
-          };
+        /**
+         * update our currentTime when the audioContext.currentTime is updated
+         */
+        $scope.$watch(function () {
+          return $scope.audioContext.currentTime;
+        }, function () {
+          $scope.currentTime = $scope.audioContext.currentTime - $scope.offset;
+        });
 
-          /**
-           * called when the global volume or one of the tracks volumes is changed
-           */
-          $scope.volumeChanged = function (track) {
-            if(track && !track.muted){
-              track.gainNode.gain.value = track.volume * $scope.globalVolume;
-              return;
-            }
-            for (var i = 0; i < $scope.tracks.length; i++) {
-              if ($scope.tracks[i].muted) {
-                $scope.tracks[i].gainNode.gain.value = 0;
-              } else {
-                $scope.tracks[i].gainNode.gain.value = $scope.tracks[i].volume * $scope.globalVolume;
-              }
-            }
-          };
-
-          $scope.mute = function(track){
-            track.muted = !track.muted;
-            $scope.volumeChanged();
-          };
-
-          /**
-           * called when the user "travels through time"
-           * @param timeSelected
-           */
-          $scope.timeChanged = function (timeSelected) {
-            for (var i = 0; i < $scope.tracks.length; i++) {
-              $scope.tracks[i].source.stop();
-              var buffer = $scope.tracks[i].source.buffer;
-              $scope.tracks[i].source = $scope.audioContext.createBufferSource();
-              $scope.tracks[i].source.buffer = buffer;
-              $scope.tracks[i].source.connect($scope.tracks[i].gainNode);
-              $scope.tracks[i].source.start(0, timeSelected);
-              $scope.currentTime = timeSelected;
-              $scope.offset = $scope.audioContext.currentTime - $scope.currentTime;
-            }
-          };
-
-          /**
-           * called when the user wants to listen one single track
-           * @param track
-           */
-          $scope.silenceAskedFrom = function (track) {
-            var silence = false;
-            if(!track.muted) {
-              for (var i = 0; i < $scope.tracks.length; i++) {
-                if ($scope.tracks[i] != track && !$scope.tracks[i].muted) {
-                  silence = true;
-                  break;
-                }
-              }
-            }else{
-              silence = true;
-            }
-            track.muted = false;
-            for (var i = 0; i < $scope.tracks.length; i++) {
-              if ($scope.tracks[i] === track)continue;
-              $scope.tracks[i].muted = silence;
-            }
-            $scope.volumeChanged();
-          };
-
-          /**
-           * update our currentTime when the audioContext.currentTime is updated
-           */
-          $scope.$watch(function () {
-            return $scope.audioContext.currentTime;
-          }, function () {
-            $scope.currentTime = $scope.audioContext.currentTime - $scope.offset;
-          });
+        $scope.$on("$destroy", function () {
+          $scope.audioContext.close();
+          $scope.audioContext = undefined;
+          $scope.tracks = [];
+        });
 
         // update display of things - makes time-scrub work
         setInterval(function () {
