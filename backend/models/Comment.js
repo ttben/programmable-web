558f3761e7464fae518a095b543c4e32ec9e5a6e
maxdestors <max.destors@gmail.com>
2016-01-25 14:53:48 +0100

    normalisation Id

--- backend/models/Comment.js
+++ backend/models/Comment.js
@@ -2,7 +2,7 @@ var mongoose     = require('mongoose');
 var Schema = mongoose.Schema;
 
 var CommentSchema   = new Schema({
-	mix_id: String,
+	mixId: String,
 	authorName: String,
     text: String,
 	date : { type: Date, default: Date.now }
