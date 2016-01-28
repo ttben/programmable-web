fea6aa5d35a36b6501a0fc95aeab3ee10c997509
Garance <garance.vallat@gmail.com>
2016-01-28 09:27:58 +0100

    first error version on homepage

--- frontend/app/scripts/controllers/home.js
+++ frontend/app/scripts/controllers/home.js
@@ -8,17 +8,21 @@
  * Controller of the programmableWebApp
  */
 angular.module('programmableWebApp')
-  .controller('HomeCtrl',['$scope', 'Music', '$cookies', '$location', function ($scope, Music, $cookies, $location) {
+  .controller('HomeCtrl',['$scope', 'Music', '$cookies', '$location', 'CONSTANTS', function ($scope, Music, $cookies, $location, CONSTANTS) {
     //This cookie will store the song the user wants to play - We reset it when you get on the music list
     $cookies.remove('song');
+
     $scope.songs=[];
+    //This boolean allows us to deal with server errors for the user
     $scope.inError=false;
+
+    //We get the list of all the musics stored on the server
     Music.all(function(result) {
       $scope.songs=result;
         $scope.songs.forEach(function(song) {
           var imgURI = song.image;
           //TODO: use CONSTANTS, BUT got undefined when I tried.. :(
-          song.image = 'http://localhost:3001/' + imgURI;
+          song.image = CONSTANTS.backendUrl + imgURI;
         });
     },
     function() {
@@ -26,22 +30,19 @@ angular.module('programmableWebApp')
       $scope.inError=true;
     });
 
+    //When we click on a specific song, we get all its specific data, and move on to the next page
   $scope.getSpecificSong = function(songName) {
     Music.get(songName, function(successAnswer) {
       $cookies.music = successAnswer.data;
-      //$window.location.href = pathIWant;
-      $cookies.music.image = 'http://localhost:3001/' + $cookies.music.image;
+      $cookies.music.image = CONSTANTS.backendUrl + $cookies.music.image;
       $location.url('/musicPlayer');
     }, function() {
+      $scope.inError=true;
       console.log('in error :((');
     });
   };
     // default view is cards
     $scope.mode = 2;
-
-
-
-
 
 
   }]);
