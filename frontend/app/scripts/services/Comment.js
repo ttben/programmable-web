56bf7e3f01c90ef257da8e3f727ba7c9203d91e1
Garance <garance.vallat@gmail.com>
2016-02-04 08:00:40 +0100

    comments on all services

--- frontend/app/scripts/services/Comment.js
+++ frontend/app/scripts/services/Comment.js
@@ -1,39 +1,53 @@
 'use strict';
 /**
  * Created by Garance on 04/01/2016.
+ * This service is used for every communication regarding a mix's comments.
  */
-angular.module('programmableWebApp') 
-  .factory('Comment', ['CONSTANTS', '$http', '$cookies', function(CONSTANTS, $http, $cookies) {
-    var obj = {
-      newC: function(mixId, comment, successCB, failCB) {
+angular.module('programmableWebApp')
+  .factory('Comment', ['CONSTANTS', '$http', '$cookies', function (CONSTANTS, $http, $cookies) {
+    return {
+
+      /**
+       * newC: to post a new comment on the server
+       * @param mixId the mixId the comment is linked to
+       * @param comment the content of the comment
+       * @param successCB the success function
+       * @param failCB the error function
+       */
+      newC: function (mixId, comment, successCB, failCB) {
         $http({
           method: 'POST',
-          url: CONSTANTS.backendUrl + CONSTANTS.com+'/?token='+$cookies.get('token'),
+          url: CONSTANTS.backendUrl + CONSTANTS.com + '/?token=' + $cookies.get('token'),
           headers: {'Content-Type': 'application/json; charset=UTF-8'},
-          data: { "authorName": $cookies.get('userName'),
+          data: {
+            "authorName": $cookies.get('userName'),
             "mixId": mixId,
             "text": comment
           }
         }).then(function (data) {
           successCB(data);
-        }, function(error) {
+        }, function (error) {
           console.log(error.data);
           failCB(error);
         });
       },
-      //          Music.loadMix(theMix.id, function(tracks) {
-      loadMix: function(mixId, successCB, failCB) {
+      /**
+       * loadMix: to load all the parameters of a given mix
+       * @param mixId the mixId to load
+       * @param successCB the success function
+       * @param failCB the error function
+       */
+      loadMix: function (mixId, successCB, failCB) {
         $http({
           method: 'GET',
-          url: CONSTANTS.backendUrl + CONSTANTS.mix+'/?token='+$cookies.get('token')+'&mixID='+mixId,
+          url: CONSTANTS.backendUrl + CONSTANTS.mix + '/?token=' + $cookies.get('token') + '&mixID=' + mixId,
           headers: {'Content-Type': 'application/json; charset=UTF-8'}
         }).then(function (data) {
           successCB(data);
-        }, function(error) {
+        }, function (error) {
           console.log(error.data);
           failCB(error);
         });
       }
     };
-    return obj;
   }]);
--- frontend/app/scripts/services/Constants.js
+++ frontend/app/scripts/services/Constants.js
@@ -8,14 +8,14 @@
  * @name FrontendApp.service:CONSTANTS
  * @description
  * # BCONSTANTS
- * Service of the FrontendApp
+ * Service of the FrontendApp, with all the constants used in the app for client / server communication
  */
 angular.module('programmableWebApp')
   .constant('CONSTANTS', {
     backendUrl: 'http://localhost:3001/',
-    music:'songs',
-    auth:'authenticate',
-    mix:'mixes',
-    com:'comments',
+    music: 'songs',
+    auth: 'authenticate',
+    mix: 'mixes',
+    com: 'comments',
     rating: 'rating'
   });
